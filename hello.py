a165faa850e815fb5b473e06ea7ced54ce42f462
Priyani <priyanirrawal@gmail.com>
2025-06-04 15:53:19 -0400

    numbering and deleting tasks

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
+lines = 0
 i=1
 while i>0:
-    user = input("What would you like to do? Input a Number: \n 1. Add a task \n 2. Show tasks \n 3. Count the number of tasks \n 4. Exit\n")
+    user = input("What would you like to do? Input a Number: \n 1. Add a task \n 2. Show tasks \n 3. Count the number of tasks \n 4. Delete task \n 5. Exit\n")
     if user == "1":
         creation = input("What is your task?")
+        with open("tasks.txt", "r") as f:
+            lines = len(f.readlines())
         with open("tasks.txt", "a") as f:
-            f.write(creation+"\n")
+            f.write(str(lines+1)+". "+creation+"\n")
             print('Task was added.')
     elif user == "2":
         with open("tasks.txt", "r") as f:
@@ -12,8 +15,19 @@ while i>0:
             print(content)
     elif user == "3":
         with open("tasks.txt", "r") as f:
-            lines = f.readlines()
-            print("The number of tasks in your list are:", len(lines))
+            total_lines = f.readlines()
+            print("The number of tasks in your list are:", len(total_lines))
+    elif user == "4":
+        with open("tasks.txt", "r") as f:
+            num_tasks = f.readlines()
+            deletion = int(input("What number task do you want to delete?\n"))
+            if 1 <= deletion <= len(num_tasks):
+                del num_tasks[deletion-1]
+                print("Task number",deletion,"was deleted.")
+                with open("tasks.txt", "w") as f:
+                    f.writelines(num_tasks)
+            else:
+                print("Invalid task number.")
     else:
         print('Exiting.')
         break
