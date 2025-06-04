e61b065d16eda73f595dde460d9a54efcd322404
Priyani <priyanirrawal@gmail.com>
2025-06-04 13:19:19 -0400

    added loop and more options

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-user = input("Write down a task.")
-with open("tasks.txt", "a") as f:
-    f.write(user+"\n")
-with open("tasks.txt", "r") as f:
-    content = f.read()
-print(content)
+i=1
+while i>0:
+    user = input("What would you like to do? Input a Number: \n 1. Add a task \n 2. Show tasks \n 3. Exit")
+    if user == "1":
+        creation = input("What is your task?")
+        with open("tasks.txt", "a") as f:
+            f.write(creation+"\n")
+            print('Task was added.')
+    elif user == "2":
+        with open("tasks.txt", "r") as f:
+            content = f.read()
+            print(content)
+    else:
+        print('Exiting.')
+        break
