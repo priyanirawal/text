b5c5e096d940f1cbe39e66267995b177bb42f39d
Priyani <priyanirrawal@gmail.com>
2025-06-04 14:44:44 -0400

    new feature added

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 i=1
 while i>0:
-    user = input("What would you like to do? Input a Number: \n 1. Add a task \n 2. Show tasks \n 3. Exit")
+    user = input("What would you like to do? Input a Number: \n 1. Add a task \n 2. Show tasks \n 3. Count the number of tasks \n 4. Exit\n")
     if user == "1":
         creation = input("What is your task?")
         with open("tasks.txt", "a") as f:
@@ -10,6 +10,10 @@ while i>0:
         with open("tasks.txt", "r") as f:
             content = f.read()
             print(content)
+    elif user == "3":
+        with open("tasks.txt", "r") as f:
+            lines = f.readlines()
+            print("The number of tasks in your list are:", len(lines))
     else:
         print('Exiting.')
         break
